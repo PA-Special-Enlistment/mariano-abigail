d09fb55d92d083fc300658eebd9489c5c4f63745
Abigail MARIANO <abigail.mariano@value-exch.com>
2020-06-09 18:43:50 +0800

    Created new page for displaying of Products by Category

--- shop_ease/src/app/app.component.ts
+++ shop_ease/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ProductsByCategoryPage } from '../pages/products-by-category/products-by-category';
 
 @Component({
   templateUrl: 'app.html'
@@ -36,9 +37,13 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
+  goToHomePage() {
+    this.nav.setRoot(HomePage);
+  }
+
+  openProductsByCategoryPage() {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.setRoot(ProductsByCategoryPage);
   }
 }
--- shop_ease/src/app/app.module.ts
+++ shop_ease/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ProductsByCategoryPage } from '../pages/products-by-category/products-by-category';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ProductsByCategoryPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ProductsByCategoryPage
   ],
   providers: [
     StatusBar,
