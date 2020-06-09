da65bc7d65c1c11491b0d2a4a68ffcb3f9d4a48b
Abigail MARIANO <abigail.mariano@value-exch.com>
2020-06-09 19:27:55 +0800

    Added click event on ProductByCategory page to redirect to ProductDetail page

--- shop_ease/src/pages/products-by-category/products-by-category.ts
+++ shop_ease/src/pages/products-by-category/products-by-category.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ProductDetailsPage } from '../product-details/product-details';
 
 @Component({
   selector: 'page-products-by-category',
@@ -12,6 +13,10 @@ export class ProductsByCategoryPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProductsByCategoryPage');
+  }
+
+  openProductsPage(index) {
+    this.navCtrl.push(ProductDetailsPage, { index: index });
   }
 
 }
