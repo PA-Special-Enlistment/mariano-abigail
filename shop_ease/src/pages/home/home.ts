8f6ee6cec2404e2b6f778ba3414e205804b2c877
Abigail MARIANO <abigail.mariano@value-exch.com>
2020-06-09 14:50:56 +0800

    Built homepage and added dummy images

--- shop_ease/src/pages/home/home.ts
+++ shop_ease/src/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from "@angular/core";
+import { NavController, Slides, ToastController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  @ViewChild("productSlides") productSlides: Slides;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  ionViewDidLoad() {
+    setInterval(() => {
+      if (this.productSlides.getActiveIndex() == 3)
+        this.productSlides.slideTo(0);
+
+      this.productSlides.slideNext();
+    }, 3000);
   }
 
+  loadMoreProducts() {}
 }
