35074c23b098d115164912640d9f3d8512ba3fb7
Abigail MARIANO <abigail.mariano@value-exch.com>
2020-06-10 10:36:17 +0800

    Updated Home page. Loaded page with data.

--- shop_ease/src/pages/home/home.ts
+++ shop_ease/src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from "@angular/core";
 import { NavController, Slides, ToastController } from "ionic-angular";
 import { ProductDetailsPage } from "../product-details/product-details";
+import { Product } from "../../models/Product";
+import { ProductManager } from "../../models/ProductManager";
 
 @Component({
   selector: "page-home",
@@ -9,7 +11,18 @@ import { ProductDetailsPage } from "../product-details/product-details";
 export class HomePage {
   @ViewChild("productSlides") productSlides: Slides;
 
-  constructor(public navCtrl: NavController) {}
+  products: Product[] = [];
+  banners = [];
+
+  constructor(public navCtrl: NavController, public productManager: ProductManager) {
+    this.products = productManager.getProducts();
+    this.banners = [
+      // "https://mir-s3-cdn-cf.behance.net/project_modules/disp/56613b18954529.562d231b8203f.jpg",
+      "https://cdn.dribbble.com/users/2160766/screenshots/6776177/retouching_-_x_plr.jpg",
+      // "https://s3images.coroflot.com/user_files/individual_files/494449_8aGwx7GCIidNyrvAyQd9sfcwd.jpg"
+    ]
+    console.log(this.products)
+  }
 
   ionViewDidLoad() {
     setInterval(() => {
@@ -22,7 +35,7 @@ export class HomePage {
 
   loadMoreProducts() {}
 
-  openProductsPage(index) {
-    this.navCtrl.push(ProductDetailsPage, { index: index });
+  openProductsPage(product) {
+    this.navCtrl.push(ProductDetailsPage, { product: product });
   }
 }
